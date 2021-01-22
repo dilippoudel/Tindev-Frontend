55f8c13fd42f4f04e5f97e215cfff6942b49d985
Dilip Poudel <dilippoudel578@gmail.com>
2021-01-22 17:13:15 +0200

    updated

--- src/components/JobseekerProfileForm/profileUpload.tsx
+++ src/components/JobseekerProfileForm/profileUpload.tsx
@@ -24,7 +24,7 @@ const ProfileUpload = () => {
     data.append('pic', state.images[0])
 
     axios
-      .post('http://localhost:3000/s3/image', {
+      .post('/s3/image', {
         fileName: fileName,
         fileType: fileType,
       })
--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 import App from './App'
 import makeStore from './redux/store'
 
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'https://tindev-dev-deploy.herokuapp.com'
 const store = makeStore()
 
 ReactDOM.render(
